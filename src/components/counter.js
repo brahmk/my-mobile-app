cf7981f89a776ee9abaff829abd828d852a4ce0a
Brahm <brahm.kelly@gmail.com>
2022-05-24 15:32:12 -0400

    setup

---
+++ src/components/counter.js
@@ -0,0 +1,13 @@
+import {View, Text, Button} from 'react-native'
+
+
+export default function Counter(){
+
+    return (
+        <View >
+            <Text>you pressed me 0 times.</Text>
+        <Button title='press me'/>
+
+        </View>
+    )
+}
