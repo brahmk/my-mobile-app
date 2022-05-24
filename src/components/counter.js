ba58d8a2d06686dea08b5d9cc7d91b4383bf2901
Brahm <brahm.kelly@gmail.com>
2022-05-24 15:47:32 -0400

    styled

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,31 @@
-import {View, Text, Button} from 'react-native'
+import {View, Text, Button, StyleSheet} from 'react-native'
 
 
 export default function Counter(){
 
     return (
-        <View >
-            <Text>you pressed me 0 times.</Text>
+        <View style = {styles.counterContainer}>
+            <Text styles={styles.counterText}>you pressed me 0 times.</Text>
         <Button title='press me'/>
 
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+
+    counterContainer: {
+        width: '80vw',
+        marginLeft: '20px',
+        margin: '20px 10vw',
+        backgroundColor: 'white',
+        borderRadius: '10px',
+        borderWidth: '2px',
+        padding: 10,
+        borderColor: 'black'
+    },
+
+    counterText: {
+        fontSize: 30
+    }
+})
